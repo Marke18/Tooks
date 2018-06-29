5df6d480872cb851dfa21b3f7628e96aa66b8ccf
Unknown <luigi.marchesi01@gmail.com>
2018-06-29 10:48:01 +0200

    Modifica della parte backend

--- app/models/utent.rb
+++ app/models/utent.rb
@@ -1,6 +1,6 @@
 class Utent < ApplicationRecord
     has_many :orders
-    has_many :lines, through :orders
+    has_many :lines, through: :orders
     validates :email, presence: true, length: { minimum: 11 }
     validates :password, presence: true,  length: { minimum: 8 }
 
